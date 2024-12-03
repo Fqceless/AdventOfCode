b730d768baaebfc294c44c43b2c4a2a9e5d7d462
Christopher LaFave <clafave@cedarville.edu>
2024-12-02 23:12:14 -0500

    oops forgor to comment

--- year2024/day02/Day02.java
+++ year2024/day02/Day02.java
@@ -8,6 +8,7 @@ public class Day02 {
 		File in = new File("inputFiles\\day02.txt");
         Scanner read = new Scanner(in);
         
+        //Var decl
         int currNum;
         int nextNum;
         int difference;
@@ -15,13 +16,16 @@ public class Day02 {
         int result = 0;
         
         while(read.hasNextLine()) {
+        	//easily separate each line into all the numbers
         	String[] report = read.nextLine().split(" ");
         	
+        	//checks for increasing vs decreasing and re-inits skip
         	currNum = Integer.parseInt(report[0]);
         	nextNum = Integer.parseInt(report[1]);
         	isIncreasing = currNum < nextNum;
             boolean skip = false;
         	
+            //if a fail happens, abort the whole for loop
         	for(int i = 0; i < report.length - 1 && !skip; i++) {
         		currNum = Integer.parseInt(report[i]);
             	nextNum = Integer.parseInt(report[i + 1]);
@@ -43,6 +47,7 @@ public class Day02 {
 				File in = new File("inputFiles\\day02.txt");
 		        Scanner read = new Scanner(in);
 		        
+		        //var decl
 		        int currNum;
 		        int nextNum;
 		        int difference;
@@ -55,9 +60,11 @@ public class Day02 {
 		        	currNum = Integer.parseInt(report[0]);
 		        	nextNum = Integer.parseInt(report[1]);
 		        	isIncreasing = currNum < nextNum;
+		        	//poker joke
 		        	boolean oneTime = false;
 		            boolean skip = false;
 		        	
+		            //now there is a oneTime buffer before we init skip
 		        	for(int i = 0; i < report.length - 1 && !skip; i++) {
 		        		currNum = Integer.parseInt(report[i]);
 		            	nextNum = Integer.parseInt(report[i + 1]);
@@ -74,7 +81,7 @@ public class Day02 {
 		        	result += skip ? 0 : 1;
 		        }
 		        
-		        //585
+		        //626
 		        System.out.println("Day 02 Part 1 Result: " + result);
 		        read.close();
 	}
