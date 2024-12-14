37f563e8073f63129f5609de7224126da14853b8
Christopher LaFave <clafave@cedarville.edu>
2024-12-13 21:55:56 -0500

    ttaabbiinngg

--- year2024/day06/Day06.java
+++ year2024/day06/Day06.java
@@ -7,188 +7,188 @@ public class Day06 {
 	public static void PartOne() throws FileNotFoundException {
 		//Boiler plate for reading the text file
 		File in = new File("inputFiles\\day06.txt");
-        Scanner read = new Scanner(in);
-        
-        //Var decl
-        HashMap<String, Character> map = new HashMap<>();
-        String line = "";
-        String start = "";
-        String direction = "UP";
-        int currRow = 0;
-        int currColumn;
-        int result = 0;
-        
-        while(read.hasNext()) {
-        	line = read.next();
-        	currColumn = 0;
-        	for(Character c : line.toCharArray()) {
-        		map.put(pntToStr(currRow, currColumn), c);
-        		if(c == '^') {
-        			start = pntToStr(currRow, currColumn);
-        		}
-        		currColumn++;
-        	}
-        	currRow++;
-        }
-        
-        currRow = Integer.parseInt(start.split(",")[0]);
-        currColumn = Integer.parseInt(start.split(",")[1]);
-        map.replace(pntToStr(currRow, currColumn), '.');
-        
-        while(map.containsKey(pntToStr(currRow, currColumn))) {
-        	if(map.get(pntToStr(currRow, currColumn)) == '.') {
-        		result++;
-            	map.replace(pntToStr(currRow, currColumn), 'X');
-        	}
-        	
-        	if("UP".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow - 1, currColumn))
-        				&& map.get(pntToStr(currRow - 1, currColumn)) == '#') {
-        			direction = "RIGHT";
-        		}
-        		else {
-        			currRow--;
-        		}
-        	}
-        	else if("RIGHT".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow, currColumn + 1))
-        				&& map.get(pntToStr(currRow, currColumn + 1)) == '#') {
-        			direction = "DOWN";
-        		}
-        		else {
-        			currColumn++;
-        		}
-        	}
-        	else if("DOWN".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow + 1, currColumn))
-        				&& map.get(pntToStr(currRow + 1, currColumn)) == '#') {
-        			direction = "LEFT";
-        		}
-        		else {
-        			currRow++;
-        		}
-        	}
-        	else if("LEFT".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow, currColumn - 1))
-        				&& map.get(pntToStr(currRow, currColumn - 1)) == '#') {
-        			direction = "UP";
-        		}
-        		else {
-        			currColumn--;
-        		}
-        	}
-        }
-        
-        //5067
-        System.out.println("Day 06 Part 1 Result: " + result);
-        read.close();
+	        Scanner read = new Scanner(in);
+	        
+	        //Var decl
+	        HashMap<String, Character> map = new HashMap<>();
+	        String line = "";
+	        String start = "";
+	        String direction = "UP";
+	        int currRow = 0;
+	        int currColumn;
+	        int result = 0;
+	        
+	        while(read.hasNext()) {
+	        	line = read.next();
+	        	currColumn = 0;
+	        	for(Character c : line.toCharArray()) {
+	        		map.put(pntToStr(currRow, currColumn), c);
+	        		if(c == '^') {
+	        			start = pntToStr(currRow, currColumn);
+	        		}
+	        		currColumn++;
+	        	}
+	        	currRow++;
+	        }
+	        
+	        currRow = Integer.parseInt(start.split(",")[0]);
+	        currColumn = Integer.parseInt(start.split(",")[1]);
+	        map.replace(pntToStr(currRow, currColumn), '.');
+	        
+	        while(map.containsKey(pntToStr(currRow, currColumn))) {
+	        	if(map.get(pntToStr(currRow, currColumn)) == '.') {
+	        		result++;
+	            	map.replace(pntToStr(currRow, currColumn), 'X');
+	        	}
+	        	
+	        	if("UP".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow - 1, currColumn))
+	        				&& map.get(pntToStr(currRow - 1, currColumn)) == '#') {
+	        			direction = "RIGHT";
+	        		}
+	        		else {
+	        			currRow--;
+	        		}
+	        	}
+	        	else if("RIGHT".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow, currColumn + 1))
+	        				&& map.get(pntToStr(currRow, currColumn + 1)) == '#') {
+	        			direction = "DOWN";
+	        		}
+	        		else {
+	        			currColumn++;
+	        		}
+	        	}
+	        	else if("DOWN".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow + 1, currColumn))
+	        				&& map.get(pntToStr(currRow + 1, currColumn)) == '#') {
+	        			direction = "LEFT";
+	        		}
+	        		else {
+	        			currRow++;
+	        		}
+	        	}
+	        	else if("LEFT".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow, currColumn - 1))
+	        				&& map.get(pntToStr(currRow, currColumn - 1)) == '#') {
+	        			direction = "UP";
+	        		}
+	        		else {
+	        			currColumn--;
+	        		}
+	        	}
+	        }
+	        
+	        //5067
+	        System.out.println("Day 06 Part 1 Result: " + result);
+	        read.close();
 	}
 	
 	public static void PartTwo() throws FileNotFoundException {
 		//Boiler plate for reading the text file
 		File in = new File("inputFiles\\day06.txt");
-        Scanner read = new Scanner(in);
-        
-        //Var decl
-        HashMap<String, String> map = new HashMap<>();
-        String line = "";
-        String start = "";
-        String direction = "UP";
-        int currRow = 0;
-        int currColumn;
-        int result = 0;
-        
-        while(read.hasNext()) {
-        	line = read.next();
-        	currColumn = 0;
-        	for(Character c : line.toCharArray()) {
-        		map.put(pntToStr(currRow, currColumn), c.toString());
-        		if(c == '^') {
-        			start = pntToStr(currRow, currColumn);
-        		}
-        		currColumn++;
-        	}
-        	currRow++;
-        }
-        
-        currRow = Integer.parseInt(start.split(",")[0]);
-        currColumn = Integer.parseInt(start.split(",")[1]);
-        map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "W");
-        
-        while(map.containsKey(pntToStr(currRow, currColumn))) {
-        	if("UP".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow - 1, currColumn))
-        				&& map.get(pntToStr(currRow - 1, currColumn)).equals("#")) {
-        			direction = "RIGHT";
-            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "^");
-        		}
-        		else {
-            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "^");
-        			if(map.containsKey(pntToStr(currRow - 1, currColumn))
-        					&& map.get(pntToStr(currRow - 1, currColumn)).equals(".") 
-        					&& parallelUniverse(map, currRow, currColumn, "RIGHT")) {
-        				map.replace(pntToStr(currRow - 1, currColumn), map.get(pntToStr(currRow - 1, currColumn)) + "W");
-        				result++;
-        			}
-        			currRow--;
-        		}
-        	}
-        	else if("RIGHT".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow, currColumn + 1))
-        				&& map.get(pntToStr(currRow, currColumn + 1)).contains("#")) {
-        			direction = "DOWN";
-            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + ">");
-        		}
-        		else {
-            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + ">");
-        			if(map.containsKey(pntToStr(currRow, currColumn + 1)) 
-        					&& map.get(pntToStr(currRow, currColumn + 1)).equals(".")
-        					&& parallelUniverse(map, currRow, currColumn, "DOWN")) {
-        				map.replace(pntToStr(currRow, currColumn + 1), map.get(pntToStr(currRow, currColumn + 1)) + "W");
-        				result++;
-        			}
-        			currColumn++;
-        		}
-        	}
-        	else if("DOWN".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow + 1, currColumn))
-        				&& map.get(pntToStr(currRow + 1, currColumn)).contains("#")) {
-        			direction = "LEFT";
-            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "V");
-        		}
-        		else {
-            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "V");
-        			if(map.containsKey(pntToStr(currRow + 1, currColumn))
-        					&& map.get(pntToStr(currRow + 1, currColumn)).equals(".")
-        					&& parallelUniverse(map, currRow, currColumn, "LEFT")) {
-        				map.replace(pntToStr(currRow + 1, currColumn), map.get(pntToStr(currRow + 1, currColumn)) + "W");
-        				result++;
-        			}
-        			currRow++;
-        		}
-        	}
-        	else if("LEFT".equals(direction)) {
-        		if(map.containsKey(pntToStr(currRow, currColumn - 1))
-        				&& map.get(pntToStr(currRow, currColumn - 1)).contains("#")) {
-        			direction = "UP";
-        			map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "<");
-        		}
-        		else {
-        			map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "<");
-        			if(map.containsKey(pntToStr(currRow, currColumn - 1))
-        					&& map.get(pntToStr(currRow, currColumn - 1)).equals(".")
-        					&& parallelUniverse(map, currRow, currColumn, "UP")) {
-        				map.replace(pntToStr(currRow, currColumn - 1), map.get(pntToStr(currRow, currColumn - 1)) + "W");
-        				result++;
-        			}
-        			currColumn--;
-        		}
-        	}
-        }
-	
-        //1793
-        System.out.println("Day 06 Part 2 Result: " + result);
-        read.close();
+	        Scanner read = new Scanner(in);
+	        
+	        //Var decl
+	        HashMap<String, String> map = new HashMap<>();
+	        String line = "";
+	        String start = "";
+	        String direction = "UP";
+	        int currRow = 0;
+	        int currColumn;
+	        int result = 0;
+	        
+	        while(read.hasNext()) {
+	        	line = read.next();
+	        	currColumn = 0;
+	        	for(Character c : line.toCharArray()) {
+	        		map.put(pntToStr(currRow, currColumn), c.toString());
+	        		if(c == '^') {
+	        			start = pntToStr(currRow, currColumn);
+	        		}
+	        		currColumn++;
+	        	}
+	        	currRow++;
+	        }
+	        
+	        currRow = Integer.parseInt(start.split(",")[0]);
+	        currColumn = Integer.parseInt(start.split(",")[1]);
+	        map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "W");
+	        
+	        while(map.containsKey(pntToStr(currRow, currColumn))) {
+	        	if("UP".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow - 1, currColumn))
+	        				&& map.get(pntToStr(currRow - 1, currColumn)).equals("#")) {
+	        			direction = "RIGHT";
+	            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "^");
+	        		}
+	        		else {
+	            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "^");
+	        			if(map.containsKey(pntToStr(currRow - 1, currColumn))
+	        					&& map.get(pntToStr(currRow - 1, currColumn)).equals(".") 
+	        					&& parallelUniverse(map, currRow, currColumn, "RIGHT")) {
+	        				map.replace(pntToStr(currRow - 1, currColumn), map.get(pntToStr(currRow - 1, currColumn)) + "W");
+	        				result++;
+	        			}
+	        			currRow--;
+	        		}
+	        	}
+	        	else if("RIGHT".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow, currColumn + 1))
+	        				&& map.get(pntToStr(currRow, currColumn + 1)).contains("#")) {
+	        			direction = "DOWN";
+	            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + ">");
+	        		}
+	        		else {
+	            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + ">");
+	        			if(map.containsKey(pntToStr(currRow, currColumn + 1)) 
+	        					&& map.get(pntToStr(currRow, currColumn + 1)).equals(".")
+	        					&& parallelUniverse(map, currRow, currColumn, "DOWN")) {
+	        				map.replace(pntToStr(currRow, currColumn + 1), map.get(pntToStr(currRow, currColumn + 1)) + "W");
+	        				result++;
+	        			}
+	        			currColumn++;
+	        		}
+	        	}
+	        	else if("DOWN".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow + 1, currColumn))
+	        				&& map.get(pntToStr(currRow + 1, currColumn)).contains("#")) {
+	        			direction = "LEFT";
+	            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "V");
+	        		}
+	        		else {
+	            		map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "V");
+	        			if(map.containsKey(pntToStr(currRow + 1, currColumn))
+	        					&& map.get(pntToStr(currRow + 1, currColumn)).equals(".")
+	        					&& parallelUniverse(map, currRow, currColumn, "LEFT")) {
+	        				map.replace(pntToStr(currRow + 1, currColumn), map.get(pntToStr(currRow + 1, currColumn)) + "W");
+	        				result++;
+	        			}
+	        			currRow++;
+	        		}
+	        	}
+	        	else if("LEFT".equals(direction)) {
+	        		if(map.containsKey(pntToStr(currRow, currColumn - 1))
+	        				&& map.get(pntToStr(currRow, currColumn - 1)).contains("#")) {
+	        			direction = "UP";
+	        			map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "<");
+	        		}
+	        		else {
+	        			map.replace(pntToStr(currRow, currColumn), map.get(pntToStr(currRow, currColumn)) + "<");
+	        			if(map.containsKey(pntToStr(currRow, currColumn - 1))
+	        					&& map.get(pntToStr(currRow, currColumn - 1)).equals(".")
+	        					&& parallelUniverse(map, currRow, currColumn, "UP")) {
+	        				map.replace(pntToStr(currRow, currColumn - 1), map.get(pntToStr(currRow, currColumn - 1)) + "W");
+	        				result++;
+	        			}
+	        			currColumn--;
+	        		}
+	        	}
+	        }
+		
+	        //1793
+	        System.out.println("Day 06 Part 2 Result: " + result);
+	        read.close();
 	}
 	
 	private static String pntToStr(int x, int y) {
@@ -224,7 +224,7 @@ public class Day06 {
 						return true;
 					}
 					else {
-	        			direction = "RIGHT";
+		        			direction = "RIGHT";
 						map.replace(pntToStr(row, col), map.get(pntToStr(row, col)) + "^");
 						
 					}
@@ -241,7 +241,7 @@ public class Day06 {
 						return true;
 					}
 					else {
-	        			direction = "DOWN";
+		        			direction = "DOWN";
 						map.replace(pntToStr(row, col), map.get(pntToStr(row, col)) + ">");
 					}
         		}
@@ -257,7 +257,7 @@ public class Day06 {
 						return true;
 					}
 					else {
-	        			direction = "LEFT";
+		        			direction = "LEFT";
 						map.replace(pntToStr(row, col), map.get(pntToStr(row, col)) + "V");
 					}
         		}
@@ -273,7 +273,7 @@ public class Day06 {
 						return true;
 					}
 					else {
-	        			direction = "UP";
+		        			direction = "UP";
 						map.replace(pntToStr(row, col), map.get(pntToStr(row, col)) + "<");
 					}
         		}
